5fc997d25f06581910cfed184363f134762358e5
bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) <jacklee0223@gmail.com>
2015-07-13 00:52:11 -0700

    BGM Random

--- js/main.js
+++ js/main.js
@@ -194,10 +194,10 @@ var audio;
 var playlist;
 var tracks;
 var current;
-var ul = document.querySelector('ul');
-for (var i = ul.children.length; i >= 0; i--) {
-    ul.appendChild(ul.children[Math.random() * i | 0]);
-}
+// var ul = document.querySelector('ul');
+// for (var i = ul.children.length; i >= 0; i--) {
+//     ul.appendChild(ul.children[Math.random() * i | 0]);
+// }
 
 
 init();
